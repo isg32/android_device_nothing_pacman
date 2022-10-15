798d1eede48b1baf72f25eec5e91d4a25dc8da81
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    pacman: Rework way to prevent adding duplicate dependencies
    Change-Id: I2eb6484dbe3a9fddc883bf1290da1b76c4e1c54c

--- extract-files.sh
+++ extract-files.sh
@@ -56,7 +56,7 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic)
-            "${PATCHELF}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
+            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
     esac
 }
