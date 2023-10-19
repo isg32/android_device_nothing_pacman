8da3d107ba526372759ead67a85514d2347cc911
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    pacman: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+PRODUCT_PACKAGES += \
+    libdrm.vendor \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
