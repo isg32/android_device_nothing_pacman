b21a8ea45d9ca850db7e569e09efebd711ef9e01
Woomymy <woomy@woomy.be>
2023-12-08 17:13:49 +0000

    pacman: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -49,7 +49,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libshim_sink.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V2-ndk.so', 'android.hardware.security.keymint-V3-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
 }  # fmt: skip
 
