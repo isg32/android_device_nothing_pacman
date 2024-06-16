5f4d378d977b64829c2e5ff4e748f9bf3b0b9d36
Giovanni Ricca <giovanniricca@proton.me>
2024-06-16 16:36:20 +0200

    pacman: Patch libsource to load the old graphicsbuffer symbol
    Change-Id: Ic8f1880379bf7adf07193842a710a104e434d470

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib64/libsource.so)
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic)
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
