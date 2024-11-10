6018fd8bb8007fc7d64521e750659e78785115cf
AdarshGrewal <adarshgrewal@gmail.com>
2024-11-10 16:36:48 +0530

    pacman: Switch to common mediatek health aidl
    Change-Id: I09dde0e7ad897a6f1d44c43ab7fc02a40da88b21

--- device.mk
+++ device.mk
@@ -190,8 +190,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example_recovery
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
 
 # IMS
 PRODUCT_BOOT_JARS += \
