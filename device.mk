5d6be092986dcd620419679e01ddf5edad530f21
AdarshGrewal <adarshgrewal@gmail.com>
2024-11-18 21:10:46 +0530

    pacman: Import media codecs to tree
     * drop unused ones while at it also import mtk_platform_codecs_config
    
    Change-Id: I121b6245b6b67f73d969fddd3399b5bd4eb932a3

--- device.mk
+++ device.mk
@@ -198,6 +198,11 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx.vendor \
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media/mtk_platform_codecs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_platform_codecs_config.xml
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # NFC
