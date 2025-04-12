8bc5f9a0a2fa3f3cafe8071113f378cb148508d6
iamlordutkarsh <lalbaake456@gmail.com>
2025-04-12 21:28:52 +0000

    pacman: Setup FCM
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_SCREEN_DENSITY := 420
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest_vendor.xml
 
