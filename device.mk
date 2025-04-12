b42b104169c8a7427e982172ce91df8dfdbd5388
iamlordutkarsh <lalbaake456@gmail.com>
2025-04-12 20:48:22 +0000

    pacman: Setup Recovery
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Rootdir
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.mt6886:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6886 \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6886.rc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/init.recovery.mt6886.rc
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
