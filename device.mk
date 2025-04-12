47ce7ba8fe9a517e65507000dc2d155c8b278aa1
iamlordutkarsh <lalbaake456@gmail.com>
2025-04-12 20:51:41 +0000

    pacman: Inherit generic ramdisk allow list
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
