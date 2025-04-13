002e2037b09072a1d5bf5a06fdbdd5f0be404199
bengris32 <bengris32@protonmail.ch>
2025-04-13 18:30:42 +0000

    pacman: Switch to common usb and gadget service

--- device.mk
+++ device.mk
@@ -309,8 +309,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-V1-ndk.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
