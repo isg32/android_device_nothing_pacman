0eb939eb3746ce21ce8bc95d91abb74889e8cc5c
iamlordutkarsh <lalbaake456@gmail.com>
2025-04-13 07:40:18 +0000

    pacman: Enable min_refresh_rate setting
    Change-Id: I2e16b99318003de819d84cab03f7baa33b9b6f2b

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayPacman \
     FrameworkOverlayPacman \
+    LineageSettingsOverlayPacman \
     SettingsProviderOverlayPacman \
     SystemUIOverlayPacman \
     TelephonyOverlayPacman \
