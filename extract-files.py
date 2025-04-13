0a96de08cfc38bbd32b75b502adcd08c9af75d3b
iamlordutkarsh <lalbaake456@gmail.com>
2025-04-13 11:19:21 +0000

    pacman: Switch to py extract utils
    Change-Id: Ibbaf093aebc665ea954467ba7b5d3b6875b0f9b2

---
+++ extract-files.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
+        .apktool_patch('ImsService-patches/'),
+    'system_ext/lib64/libsink.so': blob_fixup()
+        .add_needed('libshim_sink.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    'vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic': blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'pacman',
+    'nothing',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
